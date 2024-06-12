ed8f0a72f87d21b8cb4723e868d0135a3cf746bf
Artem Minich <temic543.9@gmail.com>
2024-06-12 17:18:36 +0300

    Added back directory

---
+++ back/report-service/src/test/java/ua/numeric/reportservice/ReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.numeric.reportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
